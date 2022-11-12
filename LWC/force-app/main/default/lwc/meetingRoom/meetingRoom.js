fb387c5162b24d2cbdfd14d8052452d219926eb0
vudinhquang <vudinhquangk53@gmail.com>
2022-11-13 00:04:07 +0900

    Public Boolean Properties

--- LWC/force-app/main/default/lwc/meetingRoom/meetingRoom.js
+++ LWC/force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -3,4 +3,5 @@ import { LightningElement, api } from 'lwc';
 export default class MeetingRoom extends LightningElement {
     @api meetingRoomInfo = {roomName:'A-01', roomCapacity:'12'}
     
+    @api showRoomInfo = false;
 }
