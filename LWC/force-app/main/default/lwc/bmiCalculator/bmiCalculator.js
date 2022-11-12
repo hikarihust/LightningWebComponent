30c0d41253e1ca1371dd0b2e3be8358a3cb662f4
vudinhquang <vudinhquangk53@gmail.com>
2022-11-12 15:52:57 +0900

    Private Properties - Reactive

--- LWC/force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ LWC/force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -3,8 +3,23 @@ import { LightningElement, track } from 'lwc';
 export default class BmiCalculator extends LightningElement {
     cardTitle = 'BMI Calculator';
 
-    changePrivatePropertyValue(){
-        this.cardTitle = 'Changed value';
-        console.log('value: ', this.cardTitle);
+    weight;
+    height;
+
+    @track bmi;
+    onWeightChange(event){
+        this.weight = parseFloat(event.target.value);
+    }
+
+    onHeightChange(event){
+        this.height = parseFloat(event.target.value);
+    }
+
+    calculateBMI(){
+        try{
+        this.bmi = this.weight/(this.height*this.height);
+        } catch(error){
+            this.bmi = undefined;
+        }
     }
 }
