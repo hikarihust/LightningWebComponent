bce16cc4fca636a8f07a5cebafb7967c3c4ff958
vudinhquang <vudinhquangk53@gmail.com>
2023-01-07 21:43:16 +0900

    Using 3rd Party Libraries

---
+++ LWC/force-app/main/default/lwc/simpleLightBoxExample/simpleLightBoxExample.js
@@ -0,0 +1,30 @@
+import { LightningElement } from 'lwc';
+import SL from '@salesforce/resourceUrl/simplelightbox';
+import CARS from '@salesforce/resourceUrl/cars';
+import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
+
+
+export default class SimpleLightBoxExample extends LightningElement {
+
+    slLoaded = false;
+    
+    renderedCallback(){
+        if(!this.slLoaded){
+            Promise.all([
+                loadStyle(this, SL+'/simpleLightbox/dist/simpleLightbox.css'),
+                loadScript(this, SL+'/simpleLightbox/dist/simpleLightbox.js')
+            ]).then(()=>{
+                this.slLoaded = true;
+            }).catch((error)=>{
+                console.error('Could not initilize simple light box-', error);
+            });
+        }
+    }
+
+    openGallary(){
+        SimpleLightbox.open({
+            items: [CARS + '/cars/van/maruti_suzuki_eeco.jpg', CARS + '/cars/luxury/mercedes_benz_gls.jpg', CARS + '/cars/sports/Audi_R8_V10_Plus.jpg']
+        });
+    }
+
+}
