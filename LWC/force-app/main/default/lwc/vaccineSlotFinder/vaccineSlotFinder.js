e6fbe0d5b3df62df33ea698f8a21cf83a6a205f4
vudinhquang <vudinhquangk53@gmail.com>
2023-01-22 17:41:11 +0900

    Making a Callout from LWC - VSF Project

--- LWC/force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
+++ LWC/force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
@@ -3,7 +3,18 @@ import { LightningElement } from 'lwc';
 export default class VaccineSlotFinder extends LightningElement {
     centers = [];
     dates = [];
+
+    connectedCallback() {
+      const endpoint = `https://choudharymanish8585.github.io/cowin-api-sample-response/db.json`;
+      this.fetchVaccineSlots(endpoint);
+    }
     
+    async fetchVaccineSlots(endpoint) {
+      const vaccineSlotRes = await fetch(endpoint);
+      const slotsData = await vaccineSlotRes.json();
+      console.log(slotsData);
+    }
+  
     get hideMessage() {
         return false;
       }
