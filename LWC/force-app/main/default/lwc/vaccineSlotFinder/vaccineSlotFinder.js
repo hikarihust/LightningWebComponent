fd58e44e6666aa06eec4115edc21dc086581764c
vudinhquang <vudinhquangk53@gmail.com>
2023-01-24 01:54:11 +0900

    Parse and Format the Callout Response - VSF Project

--- LWC/force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
+++ LWC/force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
@@ -12,7 +12,41 @@ export default class VaccineSlotFinder extends LightningElement {
     async fetchVaccineSlots(endpoint) {
         const vaccineSlotRes = await fetch(endpoint);
         const slotsData = await vaccineSlotRes.json();
-        console.log(slotsData);
+        this.buildColumnsAndRows(slotsData.centers);
+    }
+
+    buildColumnsAndRows(data) {
+        // build columns/dates
+        const dates = new Map();
+        dates.set("name", {
+            label: "Center Name",
+            fieldName: "name",
+            type: "text"
+        });
+
+        // build rows/centers
+        const centers = new Map();
+
+        for (const center of data) {
+            !centers.has(center.center_id) &&
+                centers.set(center.center_id, { name: center.name });
+
+            for (const session of center.sessions) {
+                // destructuring syntax
+                const { date, available_capacity, min_age_limit } = session;
+
+                // add date as column in dates map
+                dates.set(date, {
+                    label: date,
+                    fieldName: date,
+                    type: "text"
+                });
+            }
+        }
+
+        console.log(dates);
+        console.log(centers);
+
     }
 
     get hideMessage() {
