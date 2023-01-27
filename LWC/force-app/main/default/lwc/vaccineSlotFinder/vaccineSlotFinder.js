ed62b93036bb468b3154575a5bf30f6b6b2fe430
vudinhquang <vudinhquangk53@gmail.com>
2023-01-28 00:43:48 +0900

    Pull Source from Default Scratch Org

--- LWC/force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
+++ LWC/force-app/main/default/lwc/vaccineSlotFinder/vaccineSlotFinder.js
@@ -4,6 +4,20 @@ export default class VaccineSlotFinder extends LightningElement {
     centers = [];
     dates = [];
 
+    pincodeChangeHandler(e) {
+        const pinCode = e.target.value;
+        const isEnterKey = e.keyCode === 13;
+        if (pinCode.length === 6 && isEnterKey) {
+            const today = new Date();
+            const formattedDate = `${today.getDate()}-${
+                today.getMonth() + 1
+            }-${today.getFullYear()}`;
+            //const endpoint = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pinCode}&date=${formattedDate}`;
+            const endpoint = `https://choudharymanish8585.github.io/cowin-api-sample-response/db.json`;
+            this.fetchVaccineSlots(endpoint);
+        }
+    }
+
     connectedCallback() {
         const endpoint = `https://choudharymanish8585.github.io/cowin-api-sample-response/db.json`;
         this.fetchVaccineSlots(endpoint);
